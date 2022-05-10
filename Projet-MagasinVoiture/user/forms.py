1b8d612f3f2f78c692d0f02c72f10136fa981b48
Mikhail KARAPETYAN <89877252+mIKIII77@users.noreply.github.com>
2022-05-10 08:52:10 +0200

    V.7
    -Format HTML and CSS documents

--- Projet-MagasinVoiture/user/forms.py
+++ Projet-MagasinVoiture/user/forms.py
@@ -30,6 +30,7 @@ class ArticleForm(ModelForm):
         elif self.instance.pk:
             self.fields['piece'].queryset = self.instance.ville.piece_set.order_by('name')
 
+
 class VilleForm(ModelForm):
     class Meta: 
         model = Ville
